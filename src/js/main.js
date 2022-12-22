af70d0a0a666bfb56f4c76bb62aa1c9d32f7626d
Nitin Mittal <nitin2000mittal@gmail.com>
2022-12-22 13:33:43 +0530

    commit minor changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,62 @@
 const floors = document.querySelector("#enterFloors");
 const lifts = document.querySelector("#enterLifts");
 const generateBtn = document.querySelector("#generateBtn");
-
+const inputSection = document.querySelector("#inputSection");
+const floor_section = document.querySelector(".floor_section")
 
 function clickHandler(){
-    for(let i=1;i<=floors.value;i++){
-        const floor = document.createElement("div");
-        floor.className = "floor";
+    inputSection.style.display = "none";
+    for(let i=floors.value;i>=1;i--){
 
-        const upButton = documnet.createElement("button");
-        upButton.className = "upButton";
+        const btn = document.createElement("div");
+        btn.setAttribute("class","btn");
+        floor_section.appendChild(btn);
+        
+        if(i>1){
+        const upButton = document.createElement("button");
+        upButton.setAttribute("class","upButton");
+        upButton.innerText = "UP";
+        floor_section.appendChild(upButton);
+        btn.appendChild(upButton);
+        
 
-        const downButton = documnet.createElement("button");
-        downButton.className = "downButton";
+        const downButton = document.createElement("button");
+        downButton.setAttribute("class","downButton");
+        downButton.innerText = "DOWN";
+        floor_section.appendChild(downButton);
+        btn.appendChild(downButton);
+
+        }else{
+        const upButton = document.createElement("button");
+        upButton.setAttribute("class","upButton");
+        upButton.innerText = "UP";
+        floor_section.appendChild(upButton);
+        btn.appendChild(upButton);
+
+        }
+
+        
+
 
         const level = document.createElement("div");
-        level.className = "level";
-
+        level.setAttribute("class","level");
+        floor_section.appendChild(level);
         level.innerText = "Floor" + i;
+
+        const floor = document.createElement("div");
+        floor.setAttribute("class","floor");
+        floor_section.appendChild(floor);
 
 
     }
 
     for(let i=0;i<lifts.value;i++){
         const lift = document.createElement("div");
-
-        lift.className = "lift";
+        floor_section.appendChild(lift);
+        lift.setAttribute("class","lift");
     }
+
+
 }
 
 generateBtn.addEventListener("click",clickHandler);
